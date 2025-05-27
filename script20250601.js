899b827ac87dc9b4bcc7f858469c0fca3e1c9a74
sakheverlee <168533060+sakheverlee@users.noreply.github.com>
2025-05-27 19:04:19 +0200

    Update script20250601.js

--- script20250601.js
+++ script20250601.js
@@ -1,15 +1,15 @@
 let data20250601 = [
-    ['nummer',        'coria ',                                                                                          'pdf' , 'web' , 'mp3' , 'titel' ],
-    ['0916: 1-2-3',   '<a href=./0916_GeproefdGeleefdHerkend.html> <img src="./quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Geproefd, geleefd herkend (Geen ander brood)'  ],
-    ['0035',          '<a href=./0035_KwaadEnZorgen.html>          <img src="./quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Kwaad en zorgen'                               ],
-    ['0266',          '<a href=./0266_Here_der_Heren.html>         <img src="./quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Here der heren'                                ],
-    ['0227: 1-2',     '',                                                                                                        ' ', ' ' , '' , 'Woorden verdwijnen'                            ],
-    ['0381',          '<a href=./0381_Thuma_Mina.html>             <img src="./quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Thuma Mina: Rond uw woord'                     ],
-    ['0377',          '',                                                                                                        ' ', ' ' , '' , 'Alles wat u prijzen kan'                       ],
-    ['1612: 1',       '<a href=0035_KwaadEnZorgen.html>            <img src="./quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Kwaad en zorgen'                               ],
-    ['0338: 1-2-3',   '',                                                                                                        ' ', ' ' , '' , 'Jou ontmoeten'                                 ],
-    ['1484',          '<a href=./1484_OpdatUwDroom.html>           <img src="./quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Opdat uw droom'                                ],
-    ['0244: 1-2-3-4', '<a href=./0244_WakendUitzien.html>          <img src="./quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Waken uitzien'                                 ]
+    ['nummer', 'strofen',       'coria ',                                                                                          'pdf' , 'web' , 'mp3' , 'titel' ],
+    ['0916', '1-2-3',   '<a href=./0916_GeproefdGeleefdHerkend.html> <img src="./quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Geproefd, geleefd herkend (Geen ander brood)'  ],
+    ['0035', 'alles',   '<a href=./0035_KwaadEnZorgen.html>          <img src="./quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Kwaad en zorgen'                               ],
+    ['0266', 'alles',   '<a href=./0266_Here_der_Heren.html>         <img src="./quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Here der heren'                                ],
+    ['0227', '1-2',     '',                                                                                                        ' ', ' ' , '' , 'Woorden verdwijnen'                            ],
+    ['0381', 'alles',   '<a href=./0381_Thuma_Mina.html>             <img src="./quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Thuma Mina: Rond uw woord'                     ],
+    ['0377', 'alles',   '',                                                                                                        ' ', ' ' , '' , 'Alles wat u prijzen kan'                       ],
+    ['1612', '1',       '<a href=0035_KwaadEnZorgen.html>            <img src="./quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Kwaad en zorgen'                               ],
+    ['0338', '1-2-3',   '',                                                                                                        ' ', ' ' , '' , 'Jou ontmoeten'                                 ],
+    ['1484', 'alles',   '<a href=./1484_OpdatUwDroom.html>           <img src="./quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Opdat uw droom'                                ],
+    ['0244', '1-2-3-4', '<a href=./0244_WakendUitzien.html>          <img src="./quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Waken uitzien'                                 ]
 ];
           
 function createTable(data20250601) {
