39a83bb3525b6e09b7ac4598a0b56f1f26252995
sakheverlee <168533060+sakheverlee@users.noreply.github.com>
2025-06-02 15:53:11 +0200

    Create script20250629.js

---
+++ src/script20250629.js
@@ -0,0 +1,26 @@
+let data20250629 = [
+    ['nummer', 'strofen',       'coria ',                                                                                          'pdf' , 'web' , 'mp3' , 'titel' ],
+    ['0478', '1-2-3',   '<a href=coria/0478_Danklied.html>                   <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Danklied'                             ],
+    ['0384', 'alles',   '',  ' ', ' ' , '' , 'Mokonzi in het Nederlands'            ],
+    ['0731', '1-2-3',   '',  ' ', ' ' , '' , 'Zing voor de Heer                     ],
+    ['0865', '1-2',     '',  ' ', ' ' , '' , 'Heb dank'                             ],
+    ['1609', 'alles',   '<a href=coria/1609_Dank_U_Heer.html>                <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Dank u Heer'                          ],
+    ['0539', 'alles',   '',  ' ', ' ' , '' , 'Heilig'                               ],
+    ['0006', 'alles',   '',  ' ', ' ' , '' , 'Heer, verlicht ons hart'              ],
+    ['1655', '1-2-3',   '<a href=coria/1655_Het_lied_van_samen.html>         <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Het lied van samen'                   ],
+    ['0412', 'alles',   '<a href=coria/0412_WaarMensenElkaarDragen.html>     <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Waar mensen elkaar dragen'            ],
+    ['0917', 'alles',   '<a href=coria/0917_OldIrishBlessing.html>           <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Old Irish blessing'                   ]
+];
+          
+function createTable(data20250629) {
+  const [headings, ...rows] = data20250629;
+      
+    return `
+          <table>
+            <thead>${getCells(headings, 'th')}</thead>
+            <tbody>${rows.map(getRow).join('')}</tbody>
+          </table>
+    `;
+}
+
+document.body.insertAdjacentHTML('beforeend', createTable(data20250629));
