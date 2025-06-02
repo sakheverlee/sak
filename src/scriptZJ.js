9c748163ef5183b381883f22180fbb0f92ea5f86
BartSymbaloo <79929985+BartSymbaloo@users.noreply.github.com>
2025-06-02 19:54:06 +0200

    Nieuwe bestanden

--- src/scriptZJ.js
+++ src/scriptZJ.js
@@ -2,7 +2,8 @@
 let dataZJ = [
     ['nummer',  'coria ', 'pdf' , 'web' , 'mp3' , 'titel' ],
     ['ZJ 306',  ' ', ' ' , '<a href=https://www.youtube.com/watch?v=PlJZtPhT15A> <img src="src/world-wide-web.png" width="20" height="20"> </a>', '' , 'Een mens te zijn op aarde'       ],
-    ['ZJ 331',  ' ', ' ' , '<a href=https://www.youtube.com/watch?v=Kkz_14GR_D8> <img src="src/world-wide-web.png" width="20" height="20"> </a>', '' , 'Jezus diep in de woestijn'       ]
+    ['ZJ 331',  ' ', ' ' , '<a href=https://www.youtube.com/watch?v=Kkz_14GR_D8> <img src="src/world-wide-web.png" width="20" height="20"> </a>', '' , 'Jezus diep in de woestijn'       ],
+    ['ZJ 936',  ' ', ' ' , '<a https://www.youtube.com/watch?v=v_ryzBavlQA>      <img src="src/world-wide-web.png" width="20" height="20"> </a>', '' , 'Wie Gods rechte wegen gaat'      ]
 ];
           
 function createTable(dataZJ) {
