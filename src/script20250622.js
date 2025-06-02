ea8eb9aa7e9195590c42b1d7a2c4a5beeb5e83d6
sakheverlee <168533060+sakheverlee@users.noreply.github.com>
2025-06-02 16:27:18 +0200

    Update script20250622.js

--- src/script20250622.js
+++ src/script20250622.js
@@ -1,9 +1,9 @@
 let data20250622 = [
     ['nummer', 'strofen',       'coria ',                                                                                          'pdf' , 'web' , 'mp3' , 'titel' ],
-    ['0001', '1-2-4',   '<a href=coria/0916_GeproefdGeleefdHerkend.html>     <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Wie Gods rechte wegen gaat'           ],
+    ['0001', '1-2-4',   ',  ' ', ' ' , '' , 'Wie Gods rechte wegen gaat'           ],
     ['1799', 'alles',   '<a href=coria/1799_Toon_ons_uw_Zoon.html>           <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Toon ons uw zoon'                     ],
     ['0112', 'alles',   '<a href=coria/0112_GloriaSeiDirGesungen.html>       <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Gloria sei dir gesungen               ],
-    ['0373', '1-4-5',   '<a href=coria/0373_ZONNELIED_VAN_FRANCISCUS>        <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Zonnelied van Franciscus verdwijnen'  ],
+    ['0373', '1-4-5',   '<a href=coria/0373_ZONNELIED_VAN_FRANCISCUS.html>   <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Zonnelied van Franciscus verdwijnen'  ],
     ['0609', 'alles',   '<a href=coria/0609_Tussenzang_Laten_wij_zijn.html>  <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Laten wij zijn als de levende stenen' ],
     ['1507', 'alles',   '<a href=coria/1507_Heilig.html>                     <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Heilig'                               ],
     ['1802', '1',       '<a href=coria/1802_ElHimnoDeLaPaz(Chili).html>      <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Paz paz paz'                          ],
