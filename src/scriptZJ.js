54adb4c6cb2e66d5016daaa8441941e4ba350dc5
sakheverlee <168533060+sakheverlee@users.noreply.github.com>
2025-09-12 21:12:39 +0200

    Update scriptZJ.js

--- src/scriptZJ.js
+++ src/scriptZJ.js
@@ -3,6 +3,7 @@ let dataZJ = [
     ['nummer',  'coria ', 'pdf' , 'web' , 'mp3' , 'titel' ],
     ['ZJ 306',  ' ', ' ' , '<a href=https://www.youtube.com/watch?v=PlJZtPhT15A> <img src="src/world-wide-web.png" width="20" height="20"> </a>', '' , 'Een mens te zijn op aarde'       ],
     ['ZJ 331',  ' ', ' ' , '<a href=https://www.youtube.com/watch?v=Kkz_14GR_D8> <img src="src/world-wide-web.png" width="20" height="20"> </a>', '' , 'Jezus diep in de woestijn'       ],
+    ['ZJ 412',  ' ', ' ' , '<a href=https://www.youtube.com/watch?v=Uvhc6AMWaTE> <img src="src/world-wide-web.png" width="20" height="20"> </a>', '' , 'Al heeft hij ons verlaten'       ],
     ['ZJ 936',  ' ', ' ' , '<a https://www.youtube.com/watch?v=v_ryzBavlQA>      <img src="src/world-wide-web.png" width="20" height="20"> </a>', '' , 'Wie Gods rechte wegen gaat'      ]
 ];
           
