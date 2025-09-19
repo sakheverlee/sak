0a5421a8d5cb12bfe90b14f844805c8b9a8e4130
sakheverlee <168533060+sakheverlee@users.noreply.github.com>
2025-09-19 11:42:28 +0200

    Update script20250928.js

--- src/script20250928.js
+++ src/script20250928.js
@@ -1,15 +1,15 @@
 let data20250928 = [
     ['nummer', 'strofen',       'coria ',                                                                                          'pdf' , 'web' , 'mp3' , 'titel' ],
-    ['1876', '1-2-3',   '<a href=coria/missing.html>  <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'ONTBREEKT: Worden we vrij'                       ],
+    ['1876', '1-2-3',   '',  ' ', ' ' , '' , 'ONTBREEKT: Worden we vrij'                       ],
     ['0035', 'alles',   '<a href=coria/0035_KwaadEnZorgen.html>  <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Kwaad en zorgen'                      ],
     ['0587', '1-2-3',   '<a href=coria/0587_EerAanGodInDenHoge.html>  <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Eer aan God in den hoge'              ],
-    ['0396', '1-2-3-4', '<a href=coria/missing.html>  <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'ONTBREEKT: Wij kunnen de ogen niet sluiten'      ],
+    ['0396', '1-2-3-4', '',  ' ', ' ' , '' , 'ONTBREEKT: Wij kunnen de ogen niet sluiten'      ],
     ['0609', 'alles',   '<a href=coria/0609_Tussenzang_Laten_wij_zijn.html>  <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Laten we zijn als de levende stenen'  ],
     ['1507', 'alles',   '<a href=coria/1507_Heilig.html>  <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Driemaal heilig'                      ],
     ['1612', '1',       '<a href=coria/1612_Vrede_van_God.html>  <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Vrede van God'                        ],
-    ['1735', '1-2-3',   '<a href=coria/missing.html>  <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'ONTBREEKT: De hemel is voor iedereen'            ],
+    ['1735', '1-2-3',   '',  ' ', ' ' , '' , 'ONTBREEKT: De hemel is voor iedereen'            ],
     ['1484', 'alles',   '<a href=coria/1484_OpdatUwDroom.html>  <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Opdat uw droom'                       ],
-    ['1656', '1-2-3',   '<a href=coria/missing.html>  <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'ONTBREEKT: Wat zou de wereld anders zijn'        ]
+    ['1656', '1-2-3',   '',  ' ', ' ' , '' , 'ONTBREEKT: Wat zou de wereld anders zijn'        ]
 ];
           
 function createTable(data20250928) {
