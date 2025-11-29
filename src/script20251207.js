37a2eb851261cf8e5d36db5bf515f79ffe0e6874
sakheverlee <168533060+sakheverlee@users.noreply.github.com>
2025-11-29 21:34:07 +0100

    Update script20251207.js

--- src/script20251207.js
+++ src/script20251207.js
@@ -4,7 +4,7 @@ let data20251207 = [
     ['1182', '2',         '<a href=coria/1182_HetLiedVanDeKaarsen.html>          <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Het lied van de kaarsen'     ],
     ['0349', '6-7-8',     '<a href=coria/0349_LichtInOnzeOgen.html>              <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Licht in onze ogen'          ],
     ['0769', '1-3-2',     '<a href=coria/0769_KwamVanGodswege.html>              <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Kwam van Godswege'           ],
-    ['1653', 'alles',     '<a href=coria/1653_AlleluiaBereidDeWegDesHeren.html>  <img src="src/quaver.svg" width="20" height="20"></img> </a>'',  ' ', ' ' , '' , 'Bereid de weg des Heren'    ],
+    ['1653', 'alles',     '<a href=coria/1653_AlleluiaBereidDeWegDesHeren.html>  <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Bereid de weg des Heren'    ],
     ['0537', 'alles',     '<a href=coria/0537_Heilig.html>                       <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Heilig'                      ],
     ['0690', '1-2',       '<a href=coria/0690_VredeVoorIedereen.html>            <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Vrede voor iedereen'         ],
     ['1577', '1-2',       '<a href=coria/1577_EffenDeWeg.html>                   <img src="src/quaver.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Effen de weg'                ],
