f0f3cb758d98937a8601979bac58e1fa7e11cb0c
sakheverlee <168533060+sakheverlee@users.noreply.github.com>
2025-12-11 11:42:25 +0100

    Update script20251221.js

--- src/script20251221.js
+++ src/script20251221.js
@@ -4,7 +4,7 @@ let data20251221 = [
     ['1182', '4',         '<a href=coria/1182_HetLiedVanDeKaarsen.html>          <img src="src/quaver.svg"  width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Het lied van de kaarsen'         ],
     ['0349', '6-7-8',     '<a href=coria/0349_LichtInOnzeOgen.html>              <img src="src/quaver.svg"  width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Licht in onze ogen'              ],
     ['0421', '1-2-3',     '<a href=coria/0421_JijDieBent.html>                   <img src="src/quaver.svg"  width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Jij, die bent'                   ],
-    ['0005', 'alles',     '<a href=src/0005_LaatUwLiefde.html>                   <img src="src/quaver.svg"  width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Laat uw liefde'                  ],
+    ['0005', 'alles',     '<a href=coria/0005_LaatUwLiefde.html>                 <img src="src/quaver.svg"  width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Laat uw liefde'                  ],
     ['1153', 'alles',     '<a href=coria/1153_LAUDATE_DOMINUM.html>              <img src="src/quaver.svg"  width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Laudate dominum'                 ],
     ['0415', 'alles',     '<a href=coria/0415_VredeVoorAlleMensen.html>          <img src="src/quaver.svg"  width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Vrede voor alle mensen'          ],
     ['0875', '1-2',       '<a href=src/missing.html>                             <img src="src/missing.jpg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Ik heb mijn hart tot U geheven'  ],
