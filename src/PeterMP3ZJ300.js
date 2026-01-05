847be40840c547a0bf9e62a4c42356a643a1c0a1
sakheverlee <168533060+sakheverlee@users.noreply.github.com>
2026-01-05 15:37:18 +0100

    Create PeterMP3ZJ300.js

---
+++ src/PeterMP3ZJ300.js
@@ -0,0 +1,76 @@
+let dataMP3Peter300 = [
+    ['nummer',  'mp3' , 'titel' ],
+    ['ZJ_301', '<a href=MP3_Peter/ZJ_301-Veel_hoger_dan_Abraham_klom.mp3>                   <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_301-Veel_hoger_dan_Abraham_klom'                           ],
+    ['ZJ_302', '<a href=MP3_Peter/ZJ_302-Gij_hebt_met_groot_geduld.mp3>                     <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_302-Gij_hebt_met_groot_geduld'                             ],
+    ['ZJ_303', '<a href=MP3_Peter/ZJ_303-Uit_diepten_van_ellende.mp3>                       <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_303-Uit_diepten_van_ellende'                               ],
+    ['ZJ_304', '<a href=MP3_Peter/ZJ_304-Evenals_een_moede_hinde.mp3>                       <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_304-Evenals_een_moede_hinde'                               ],
+    ['ZJ_306', '<a href=MP3_Peter/ZJ_306-Een_mens_te_zijn_op_aarde.mp3>                     <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_306-Een_mens_te_zijn_op_aarde'                             ],
+    ['ZJ_309', '<a href=MP3_Peter/ZJ_309-Zo_spreekt_de_Heer.mp3>                            <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_309-Zo_spreekt_de_Heer'                                    ],
+    ['ZJ_310', '<a href=MP3_Peter/ZJ_310-Uit_angst_en_nood.mp3>                             <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_310-Uit_angst_en_nood'                                     ],
+    ['ZJ_312', '<a href=MP3_Peter/ZJ_312-Die_vroeg_zijn_aangeworven.mp3>                    <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_312-Die_vroeg_zijn_aangeworven'                            ],
+    ['ZJ_313', '<a href=MP3_Peter/ZJ_313-Uit_de_diepten_roep_ik_U_Heer.mp3>                 <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_313-Uit_de_diepten_roep_ik_U_Heer'                         ],
+    ['ZJ_314', '<a href=MP3_Peter/ZJ_314-Uit_diepten_van_ellende.mp3>                       <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_314-Uit_diepten_van_ellende'                               ],
+    ['ZJ_315', '<a href=MP3_Peter/ZJ_315-Wij_roepen_Heer.mp3>                               <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_315-Wij_roepen_Heer'                                       ],
+    ['ZJ_316', '<a href=MP3_Peter/ZJ_316-Barmhartige_Heer.mp3>                              <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_316-Barmhartige_Heer'                                      ],
+    ['ZJ_317', '<a href=MP3_Peter/ZJ_317-Israël_trok_Egypte_uit.mp3>                        <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_317-Israël_trok_Egypte_uit'                                ],
+    ['ZJ_318', '<a href=MP3_Peter/ZJ_318-Onze_hulp_is_de_naam_van_de_Heer.mp3>              <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_318-Onze_hulp_is_de_naam_van_de_Heer'                      ],
+    ['ZJ_319', '<a href=MP3_Peter/ZJ_319-De_zonden_zijn_vergeven.mp3>                       <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_319-De_zonden_zijn_vergeven'                               ],
+    ['ZJ_320', '<a href=MP3_Peter/ZJ_320-Erbarm_U_God_en_delg_genadig.mp3>                  <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_320-Erbarm_U_God_en_delg_genadig'                          ],
+    ['ZJ_322', '<a href=MP3_Peter/ZJ_322-Uit_Oer_is_hij_getogen.mp3>                        <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_322-Uit_Oer_is_hij_getogen'                                ],
+    ['ZJ_323', '<a href=MP3_Peter/ZJ_323-Oud_het_leven_dat_wij_leiden.mp3>                  <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_323-Oud_het_leven_dat_wij_leiden'                          ],
+    ['ZJ_324', '<a href=MP3_Peter/ZJ_324-Waar_is_de_plaats_die_vrede_lacht.mp3>             <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_324-Waar_is_de_plaats_die_vrede_lacht'                     ],
+    ['ZJ_325', '<a href=MP3_Peter/ZJ_325-Die_in_de_hoogte_woont.mp3>                        <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_325-Die_in_de_hoogte_woont'                                ],
+    ['ZJ_326', '<a href=MP3_Peter/ZJ_326-Het_volg_van_de_Heer.mp3>                          <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_326-Het_volg_van_de_Heer'                                  ],
+    ['ZJ_327', '<a href=MP3_Peter/ZJ_327-Alles_wat_over_ons_geschreven_is.mp3>              <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_327-Alles_wat_over_ons_geschreven_is'                      ],
+    ['ZJ_328', '<a href=MP3_Peter/ZJ_328-Broeders_die_op_uittocht_gaan.mp3>                 <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_328-Broeders_die_op_uittocht_gaan'                         ],
+    ['ZJ_329', '<a href=MP3_Peter/ZJ_329-Wij_zoeken_hier_uw_aangezicht.mp3>                 <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_329-Wij_zoeken_hier_uw_aangezicht'                         ],
+    ['ZJ_330', '<a href=MP3_Peter/ZJ_330-Johannes_doopt_in_de_Jordaan.mp3>                  <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_330-Johannes_doopt_in_de_Jordaan'                          ],
+    ['ZJ_331', '<a href=MP3_Peter/ZJ_331-Jezus_diep_in_de_woestijn.mp3>                     <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_331-Jezus_diep_in_de_woestijn'                             ],
+    ['ZJ_351', '<a href=MP3_Peter/ZJ_351-De_koning_van_de_vrede.mp3>                        <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_351-De_koning_van_de_vrede'                                ],
+    ['ZJ_352', '<a href=MP3_Peter/ZJ_352-Naam_van_Jezus_die_ten_dode.mp3>                   <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_352-Naam_van_Jezus_die_ten_dode'                           ],
+    ['ZJ_353', '<a href=MP3_Peter/ZJ_353-Met_de_boom_des_levens.mp3>                        <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_353-Met_de_boom_des_levens'                                ],
+    ['ZJ_354', '<a href=MP3_Peter/ZJ_354-Eén_is_tot_ons_gezonden.mp3>                       <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_354-Eén_is_tot_ons_gezonden'                               ],
+    ['ZJ_355', '<a href=MP3_Peter/ZJ_355-Wie_zal_voor_God_verschijnen.mp3>                  <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_355-Wie_zal_voor_God_verschijnen'                          ],
+    ['ZJ_356', '<a href=MP3_Peter/ZJ_356-Onze_redding_uit_het_kwade.mp3>                    <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_356-Onze_redding_uit_het_kwade'                            ],
+    ['ZJ_357', '<a href=MP3_Peter/ZJ_357-Van_allen_verlaten.mp3>                            <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_357-Van_allen_verlaten'                                    ],
+    ['ZJ_358', '<a href=MP3_Peter/ZJ_358-Samenzijn_van_ons_begeren.mp3>                     <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_358-Samenzijn_van_ons_begeren'                             ],
+    ['ZJ_359', '<a href=MP3_Peter/ZJ_359-Toen_Jezus_in_zijn_uur_gekomen_was.mp3>            <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_359-Toen_Jezus_in_zijn_uur_gekomen_was'                    ],
+    ['ZJ_360', '<a href=MP3_Peter/ZJ_360-Christus_heeft_voor_ons_geleden.mp3>               <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_360-Christus_heeft_voor_ons_geleden'                       ],
+    ['ZJ_361', '<a href=MP3_Peter/ZJ_361-Mijn_volk_wat_heb_ik_u_gedaan.mp3>                 <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_361-Mijn_volk_wat_heb_ik_u_gedaan'                         ],
+    ['ZJ_362', '<a href=MP3_Peter/ZJ_362-Aanbidt_het_kruis_des_Heren.mp3>                   <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_362-Aanbidt_het_kruis_des_Heren'                           ],
+    ['ZJ_363', '<a href=MP3_Peter/ZJ_363-Dit_is_het_kruis.mp3>                              <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_363-Dit_is_het_kruis'                                      ],
+    ['ZJ_364', '<a href=MP3_Peter/ZJ_364-Jezus_Christus_is_het_beeld_van_God.mp3>           <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_364-Jezus_Christus_is_het_beeld_van_God'                   ],
+    ['ZJ_365', '<a href=MP3_Peter/ZJ_365-Glorie_lof_en_eer_aan_U.mp3>                       <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_365-Glorie_lof_en_eer_aan_U'                               ],
+    ['ZJ_366', '<a href=MP3_Peter/ZJ_366-Laat_ons_als_weleer.mp3>                           <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_366-Laat_ons_als_weleer'                                   ],
+    ['ZJ_367', '<a href=MP3_Peter/ZJ_367-Wij_roemen_in_t_Kruis_van_de_Heer.mp3>             <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_367-Wij_roemen_in_t_Kruis_van_de_Heer'                     ],
+    ['ZJ_368', '<a href=MP3_Peter/ZJ_368-God_mijn_God_waarom_hebt_Gij_mij_verlaten.mp3>     <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_368-God_mijn_God_waarom_hebt_Gij_mij_verlaten'             ],
+    ['ZJ_369', '<a href=MP3_Peter/ZJ_369-Hoort_en_ziet_het_levend_woord.mp3>                <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_369-Hoort_en_ziet_het_levend_woord'                        ],
+    ['ZJ_370', '<a href=MP3_Peter/ZJ_370-Alles_wat_over_ons_geschreven_is.mp3>              <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_370-Alles_wat_over_ons_geschreven_is'                      ],
+    ['ZJ_371', '<a href=MP3_Peter/ZJ_371-O_hoofd_vol_bloed_en_wonden.mp3>                   <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_371-O_hoofd_vol_bloed_en_wonden'                           ],
+    ['ZJ_372', '<a href=MP3_Peter/ZJ_372-Jezus_om_uw_lijden_groot.mp3>                      <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_372-Jezus_om_uw_lijden_groot'                              ],
+    ['ZJ_373', '<a href=MP3_Peter/ZJ_373-Mogen_allen_één_zijn.mp3>                          <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_373-Mogen_allen_één_zijn'                                  ],
+    ['ZJ_374', '<a href=MP3_Peter/ZJ_374-Gethsémane.mp3>                                    <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_374-Gethsémane'                                            ],
+    ['ZJ_375', '<a href=MP3_Peter/ZJ_375-Nu_valt_de_nacht.mp3>                              <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_375-Nu_valt_de_nacht'                                      ],
+    ['ZJ_376', '<a href=MP3_Peter/ZJ_376-Waarom_ben_ik_verlaten.mp3>                        <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_376-Waarom_ben_ik_verlaten'                                ],
+    ['ZJ_377', '<a href=MP3_Peter/ZJ_377-Aan_uw_stam_o_Kruis.mp3>                           <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_377-Aan_uw_stam_o_Kruis'                                   ],
+    ['ZJ_378', '<a href=MP3_Peter/ZJ_378-Stil_ligt_de_tuin.mp3>                             <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_378-Stil_ligt_de_tuin'                                     ],
+    ['ZJ_379', '<a href=MP3_Peter/ZJ_379-Door_wat_voor_grote_eenzaamheden.mp3>              <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_379-Door_wat_voor_grote_eenzaamheden'                      ],
+    ['ZJ_380', '<a href=MP3_Peter/ZJ_380-Toen_Jezus_in_zijn_uur_gekomen_was.mp3>            <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_380-Toen_Jezus_in_zijn_uur_gekomen_was'                    ],
+    ['ZJ_381', '<a href=MP3_Peter/ZJ_381-In_stille_nacht.mp3>                               <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_381-In_stille_nacht'                                       ],
+    ['ZJ_382', '<a href=MP3_Peter/ZJ_382-De_Zoon_die_voor_de_eeuwen.mp3>                    <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_382-De_Zoon_die_voor_de_eeuwen'                            ],
+    ['ZJ_383', '<a href=MP3_Peter/ZJ_383-Naast_het_kruis.mp3>                               <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_383-Naast_het_kruis'                                       ],
+    [' ',  ' ' , ' ' ]
+];
+
+function createTable(dataMP3Peter300) {
+  const [headings, ...rows] = dataMP3Peter300;
+      
+    return `
+          <table>
+            <thead>${getCells(headings, 'th')}</thead>
+            <tbody>${rows.map(getRow).join('')}</tbody>
+          </table>
+    `;
+}
+
+document.body.insertAdjacentHTML('beforeend', createTable(dataMP3Peter300));
