29447f1de8a1f37a164db0250e2c3ddd0984e2d0
BartSymbaloo <79929985+BartSymbaloo@users.noreply.github.com>
2026-01-05 19:17:25 +0100

    Wijziging PeterMP3 ZJ

---
+++ src/PeterMP3ZJ350.js
@@ -0,0 +1,50 @@
+let dataMP3Peter300 = [
+    ['nummer',  'mp3' , 'titel' ],
+    ['ZJ_351', '<a href=MP3_Peter/ZJ_351-De_koning_van_de_vrede.mp3>                        <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_351-De_koning_van_de_vrede'                                ],
+    ['ZJ_352', '<a href=MP3_Peter/ZJ_352-Naam_van_Jezus_die_ten_dode.mp3>                   <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_352-Naam_van_Jezus_die_ten_dode'                           ],
+    ['ZJ_353', '<a href=MP3_Peter/ZJ_353-Met_de_boom_des_levens.mp3>                        <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_353-Met_de_boom_des_levens'                                ],
+    ['ZJ_354', '<a href=MP3_Peter/ZJ_354-Eén_is_tot_ons_gezonden.mp3>                       <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_354-Eén_is_tot_ons_gezonden'                               ],
+    ['ZJ_355', '<a href=MP3_Peter/ZJ_355-Wie_zal_voor_God_verschijnen.mp3>                  <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_355-Wie_zal_voor_God_verschijnen'                          ],
+    ['ZJ_356', '<a href=MP3_Peter/ZJ_356-Onze_redding_uit_het_kwade.mp3>                    <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_356-Onze_redding_uit_het_kwade'                            ],
+    ['ZJ_357', '<a href=MP3_Peter/ZJ_357-Van_allen_verlaten.mp3>                            <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_357-Van_allen_verlaten'                                    ],
+    ['ZJ_358', '<a href=MP3_Peter/ZJ_358-Samenzijn_van_ons_begeren.mp3>                     <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_358-Samenzijn_van_ons_begeren'                             ],
+    ['ZJ_359', '<a href=MP3_Peter/ZJ_359-Toen_Jezus_in_zijn_uur_gekomen_was.mp3>            <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_359-Toen_Jezus_in_zijn_uur_gekomen_was'                    ],
+    ['ZJ_360', '<a href=MP3_Peter/ZJ_360-Christus_heeft_voor_ons_geleden.mp3>               <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_360-Christus_heeft_voor_ons_geleden'                       ],
+    ['ZJ_361', '<a href=MP3_Peter/ZJ_361-Mijn_volk_wat_heb_ik_u_gedaan.mp3>                 <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_361-Mijn_volk_wat_heb_ik_u_gedaan'                         ],
+    ['ZJ_362', '<a href=MP3_Peter/ZJ_362-Aanbidt_het_kruis_des_Heren.mp3>                   <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_362-Aanbidt_het_kruis_des_Heren'                           ],
+    ['ZJ_363', '<a href=MP3_Peter/ZJ_363-Dit_is_het_kruis.mp3>                              <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_363-Dit_is_het_kruis'                                      ],
+    ['ZJ_364', '<a href=MP3_Peter/ZJ_364-Jezus_Christus_is_het_beeld_van_God.mp3>           <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_364-Jezus_Christus_is_het_beeld_van_God'                   ],
+    ['ZJ_365', '<a href=MP3_Peter/ZJ_365-Glorie_lof_en_eer_aan_U.mp3>                       <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_365-Glorie_lof_en_eer_aan_U'                               ],
+    ['ZJ_366', '<a href=MP3_Peter/ZJ_366-Laat_ons_als_weleer.mp3>                           <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_366-Laat_ons_als_weleer'                                   ],
+    ['ZJ_367', '<a href=MP3_Peter/ZJ_367-Wij_roemen_in_t_Kruis_van_de_Heer.mp3>             <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_367-Wij_roemen_in_t_Kruis_van_de_Heer'                     ],
+    ['ZJ_368', '<a href=MP3_Peter/ZJ_368-God_mijn_God_waarom_hebt_Gij_mij_verlaten.mp3>     <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_368-God_mijn_God_waarom_hebt_Gij_mij_verlaten'             ],
+    ['ZJ_369', '<a href=MP3_Peter/ZJ_369-Hoort_en_ziet_het_levend_woord.mp3>                <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_369-Hoort_en_ziet_het_levend_woord'                        ],
+    ['ZJ_370', '<a href=MP3_Peter/ZJ_370-Alles_wat_over_ons_geschreven_is.mp3>              <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_370-Alles_wat_over_ons_geschreven_is'                      ],
+    ['ZJ_371', '<a href=MP3_Peter/ZJ_371-O_hoofd_vol_bloed_en_wonden.mp3>                   <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_371-O_hoofd_vol_bloed_en_wonden'                           ],
+    ['ZJ_372', '<a href=MP3_Peter/ZJ_372-Jezus_om_uw_lijden_groot.mp3>                      <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_372-Jezus_om_uw_lijden_groot'                              ],
+    ['ZJ_373', '<a href=MP3_Peter/ZJ_373-Mogen_allen_één_zijn.mp3>                          <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_373-Mogen_allen_één_zijn'                                  ],
+    ['ZJ_374', '<a href=MP3_Peter/ZJ_374-Gethsémane.mp3>                                    <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_374-Gethsémane'                                            ],
+    ['ZJ_375', '<a href=MP3_Peter/ZJ_375-Nu_valt_de_nacht.mp3>                              <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_375-Nu_valt_de_nacht'                                      ],
+    ['ZJ_376', '<a href=MP3_Peter/ZJ_376-Waarom_ben_ik_verlaten.mp3>                        <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_376-Waarom_ben_ik_verlaten'                                ],
+    ['ZJ_377', '<a href=MP3_Peter/ZJ_377-Aan_uw_stam_o_Kruis.mp3>                           <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_377-Aan_uw_stam_o_Kruis'                                   ],
+    ['ZJ_378', '<a href=MP3_Peter/ZJ_378-Stil_ligt_de_tuin.mp3>                             <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_378-Stil_ligt_de_tuin'                                     ],
+    ['ZJ_379', '<a href=MP3_Peter/ZJ_379-Door_wat_voor_grote_eenzaamheden.mp3>              <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_379-Door_wat_voor_grote_eenzaamheden'                      ],
+    ['ZJ_380', '<a href=MP3_Peter/ZJ_380-Toen_Jezus_in_zijn_uur_gekomen_was.mp3>            <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_380-Toen_Jezus_in_zijn_uur_gekomen_was'                    ],
+    ['ZJ_381', '<a href=MP3_Peter/ZJ_381-In_stille_nacht.mp3>                               <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_381-In_stille_nacht'                                       ],
+    ['ZJ_382', '<a href=MP3_Peter/ZJ_382-De_Zoon_die_voor_de_eeuwen.mp3>                    <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_382-De_Zoon_die_voor_de_eeuwen'                            ],
+    ['ZJ_383', '<a href=MP3_Peter/ZJ_383-Naast_het_kruis.mp3>                               <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_383-Naast_het_kruis'                                       ],
+    [' ',  ' ' , ' ' ]
+];
+
+function createTable(dataMP3Peter300) {
+  const [headings, ...rows] = dataMP3Peter300;
+      
+    return `
+          <table>
+            <thead>${getCells(headings, 'th')}</thead>
+            <tbody>${rows.map(getRow).join('')}</tbody>
+          </table>
+    `;
+}
+
+document.body.insertAdjacentHTML('beforeend', createTable(dataMP3Peter300));
---
+++ src/PeterMP3ZJ550.js
@@ -0,0 +1,58 @@
+let dataMP3Peter500 = [
+    ['nummer',  'mp3' , 'titel' ],
+    ['ZJ_550', '<a href=MP3_Peter/ZJ_550-Welzalig_wie_de_rechte_wegen_gaan.mp3>             <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_550-Welzalig_wie_de_rechte_wegen_gaan'                     ],
+    ['ZJ_551', '<a href=MP3_Peter/ZJ_551-De_heiligen_ons_voorgegaan.mp3>                    <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_551-De_heiligen_ons_voorgegaan'                            ],
+    ['ZJ_552', '<a href=MP3_Peter/ZJ_552-Voor_de_toegewijden.mp3>                           <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_552-Voor_de_toegewijden'                                   ],
+    ['ZJ_553', '<a href=MP3_Peter/ZJ_553-Waarlijk_dit_is_rechtvaardig.mp3>                  <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_553-Waarlijk_dit_is_rechtvaardig'                          ],
+    ['ZJ_554', '<a href=MP3_Peter/ZJ_554-God_groet_u_zuiver_bloeme.mp3>                     <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_554-God_groet_u_zuiver_bloeme'                             ],
+    ['ZJ_556', '<a href=MP3_Peter/ZJ_556-Wachters_van_de_tijd.mp3>                          <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_556-Wachters_van_de_tijd'                                  ],
+    ['ZJ_557', '<a href=MP3_Peter/ZJ_557-Maria_schone_Vrouwe.mp3>                           <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_557-Maria_schone_Vrouwe'                                   ],
+    ['ZJ_558', '<a href=MP3_Peter/ZJ_558-Maria_poort_van_Gods_genade.mp3>                   <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_558-Maria_poort_van_Gods_genade'                           ],
+    ['ZJ_559', '<a href=MP3_Peter/ZJ_559-De_Heer_heeft_mij_gezien.mp3>                      <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_559-De_Heer_heeft_mij_gezien'                              ],
+    ['ZJ_560', '<a href=MP3_Peter/ZJ_560-Zalig_zij_die_Jezus_naam_belijden.mp3>             <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_560-Zalig_zij_die_Jezus_naam_belijden'                     ],
+    ['ZJ_561', '<a href=MP3_Peter/ZJ_561-Wij_zijn_uw_volk.mp3>                              <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_561-Wij_zijn_uw_volk'                                      ],
+    ['ZJ_562', '<a href=MP3_Peter/ZJ_562-Jezus_die_langs_het_water_liep.mp3>                <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_562-Jezus_die_langs_het_water_liep'                        ],
+    ['ZJ_563', '<a href=MP3_Peter/ZJ_563-Heer_Gij_zijt_burcht_en_toren.mp3>                 <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_563-Heer_Gij_zijt_burcht_en_toren'                         ],
+    ['ZJ_563', '<a href=MP3_Peter/ZJ_563-Heer_Gij_zijt_burcht_en_toren_v2.mp3>              <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_563-Heer_Gij_zijt_burcht_en_toren_v2'                      ],
+    ['ZJ_564', '<a href=MP3_Peter/ZJ_564-Weest_niet_bezorgd.mp3>                            <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_564-Weest_niet_bezorgd'                                    ],
+    ['ZJ_565', '<a href=MP3_Peter/ZJ_565-Ik_verheerlijk_de_Heer.mp3>                        <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_565-Ik_verheerlijk_de_Heer'                                ],
+    ['ZJ_566', '<a href=MP3_Peter/ZJ_566-Hoe_is_uw_naam.mp3>                                <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_566-Hoe_is_uw_naam'                                        ],
+    ['ZJ_567', '<a href=MP3_Peter/ZJ_567-Een_smekeling_zo_kom_ik_tot_uw_troon.mp3>          <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_567-Een_smekeling_zo_kom_ik_tot_uw_troon'                  ],
+    ['ZJ_569', '<a href=MP3_Peter/ZJ_569-God_die_leven_hebt_gegeven.mp3>                    <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_569-God_die_leven_hebt_gegeven'                            ],
+    ['ZJ_570', '<a href=MP3_Peter/ZJ_570-Gij_zijt_mijn_goed_mijn_overvloed.mp3>             <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_570-Gij_zijt_mijn_goed_mijn_overvloed'                     ],
+    ['ZJ_571', '<a href=MP3_Peter/ZJ_571-Niet_als_een_storm.mp3>                            <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_571-Niet_als_een_storm'                                    ],
+    ['ZJ_572', '<a href=MP3_Peter/ZJ_572-De_aarde_is_vervuld.mp3>                           <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_572-De_aarde_is_vervuld'                                   ],
+    ['ZJ_575', '<a href=MP3_Peter/ZJ_575-Zolang_er_mensen_zijn_op_aarde.mp3>                <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_575-Zolang_er_mensen_zijn_op_aarde'                        ],
+    ['ZJ_576', '<a href=MP3_Peter/ZJ_576-Bouwen_aan_een_wereld.mp3>                         <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_576-Bouwen_aan_een_wereld'                                 ],
+    ['ZJ_578', '<a href=MP3_Peter/ZJ_578-Volk_van_God.mp3>                                  <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_578-Volk_van_God'                                          ],
+    ['ZJ_579', '<a href=MP3_Peter/ZJ_579-Uw_koninkrijk_komt.mp3>                            <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_579-Uw_koninkrijk_komt'                                    ],
+    ['ZJ_580', '<a href=MP3_Peter/ZJ_580-Zoals_een_hert_reikhalst.mp3>                      <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_580-Zoals_een_hert_reikhalst'                              ],
+    ['ZJ_581', '<a href=MP3_Peter/ZJ_581-Mijn_God_zijt_Gij.mp3>                             <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_581-Mijn_God_zijt_Gij'                                     ],
+    ['ZJ_583', '<a href=MP3_Peter/ZJ_583-Alles_wat_adem_heeft.mp3>                          <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_583-Alles_wat_adem_heeft'                                  ],
+    ['ZJ_585', '<a href=MP3_Peter/ZJ_585-Dankt_dankt_nu_allen_God.mp3>                      <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_585-Dankt_dankt_nu_allen_God'                              ],
+    ['ZJ_588', '<a href=MP3_Peter/ZJ_588-God_in_den_hoog_alleen.mp3>                        <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_588-God_in_den_hoog_alleen'                                ],
+    ['ZJ_589', '<a href=MP3_Peter/ZJ_589-De_laatsten_worden_de_eersten.mp3>                 <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_589-De_laatsten_worden_de_eersten'                         ],
+    ['ZJ_590', '<a href=MP3_Peter/ZJ_590-De_eersten_zijn_de_laatsten.mp3>                   <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_590-De_eersten_zijn_de_laatsten'                           ],
+    ['ZJ_591', '<a href=MP3_Peter/ZJ_591-Mensen_wij_zijn_geroepen_om_te_leven.mp3>          <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_591-Mensen_wij_zijn_geroepen_om_te_leven'                  ],
+    ['ZJ_593', '<a href=MP3_Peter/ZJ_593-Ga_in_het_schip.mp3>                               <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_593-Ga_in_het_schip'                                       ],
+    ['ZJ_594', '<a href=MP3_Peter/ZJ_594-Wie_oren_om_te_horen_heeft.mp3>                    <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_594-Wie_oren_om_te_horen_heeft'                            ],
+    ['ZJ_595', '<a href=MP3_Peter/ZJ_595-Mijn_ziel_maakt_groot_de_Heer.mp3>                 <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_595-Mijn_ziel_maakt_groot_de_Heer'                         ],
+    ['ZJ_596', '<a href=MP3_Peter/ZJ_596-Een_zaaier_ging_uit.mp3>                           <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_596-Een_zaaier_ging_uit'                                   ],
+    ['ZJ_597', '<a href=MP3_Peter/ZJ_597-Gij_hebt_met_uw_brede_gebaren.mp3>                 <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_597-Gij_hebt_met_uw_brede_gebaren'                         ],
+    ['ZJ_598', '<a href=MP3_Peter/ZJ_598-De_schapen_alle_honderd.mp3>                       <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_598-De_schapen_alle_honderd'                               ],
+    ['ZJ_599', '<a href=MP3_Peter/ZJ_599-Looft_uwe_God_alle_tongen_en_talen.mp3>            <img src="src/mp3.png" width="20" height="20"> </a>','ZJ_599-Looft_uwe_God_alle_tongen_en_talen'                    ],
+    [' ',  ' ' , ' ' ]
+];
+
+function createTable(dataMP3Peter500) {
+  const [headings, ...rows] = dataMP3Peter500;
+      
+    return `
+          <table>
+            <thead>${getCells(headings, 'th')}</thead>
+            <tbody>${rows.map(getRow).join('')}</tbody>
+          </table>
+    `;
+}
+
+document.body.insertAdjacentHTML('beforeend', createTable(dataMP3Peter500));
