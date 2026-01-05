c7802db84ff156bbc199121fec1b3dc38fc454b2
sakheverlee <168533060+sakheverlee@users.noreply.github.com>
2026-01-05 14:47:56 +0100

    Create Farde1300.js

---
+++ src/Farde1300.js
@@ -0,0 +1,63 @@
+let dataFarde1300 = [
+['nummer',  'coria '                                                                                                    , 'pdf'                                                                                                          , 'web'                                                                                                              , 'mp3'                                                                                      , 'titel'                           ],
+[ '1319' ,'<a href=coria/1319_HijDieGesprokenHeeft.html><img src="src/quaver.svg"width="20" height="20"></img></a>','<a href=pdf/SAK_1319-2_HijDieGesprokenHeeft.pdf><img src="src/Icon_PDF.png"width="20" height="20"></a>','' ,'', 'Hij die gesproken heeft' ],
+[ '1346' ,'<a href=coria/1346_AlsJeIntLeven.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Als je in het leven' ], 
+[ '1348' ,'<a href=coria/1348_BronInDeWoestijn.html><img src="src/quaver.svg"width="20" height="20"></img></a>','<a href=pdf/SAK_1348-2_BronInDeWoestijn.pdf><img src="src/Icon_PDF.png"width="20" height="20"></a>','' ,'', 'Bron in de woestijn' ],
+[ '1424' ,'<a href=coria/1424_GloriaGloria.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Gloria Gloria' ], 
+[ '1444' ,'<a href=coria/1444_INeedTheeEveryHour.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'I need thee every hour' ], 
+[ '1484' ,'<a href=coria/1484_OpdatUwDroom.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Opdat uw droom' ], 
+[ '1485' ,'<a href=coria/1485_KomLuisterNaarMijnWoorden.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Kom Luister naar mijn woorden' ], 
+[ '1486' ,'<a href=coria/1486_KomHeerJezus.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Kom Heer Jezus' ], 
+[ '1488' ,'<a href=coria/1488_AmbrosiaanseLofzang.html><img src="src/quaver.svg"width="20" height="20"></img></a>','<a href=pdf/SAK_1488-7_AmbrosiaanseLofzang.pdf><img src="src/Icon_PDF.png"width="20" height="20"></a>','' ,'', 'Ambrosiaanse lofzang' ],
+[ '1494' ,'<a href=coria/1494_OveralMetJezus.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Overal met Jezus' ], 
+[ '1502' ,'<a href=coria/1502_LaatZoJeLichtMaarSchijnen.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Laat zo je licht maar schijnen' ], 
+[ '1507' ,'<a href=coria/1507_Heilig.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Heilig ' ], 
+[ '1511' ,'<a href=coria/1511_HeerWeesMijnKracht.html><img src="src/quaver.svg"width="20" height="20"></img></a>','<a href=pdf/SAK_1511-4_HeerWeesMijnKracht_inD.pdf><img src="src/Icon_PDF.png"width="20" height="20"></a>','' ,'', 'Heer wees mijn kracht ' ],
+[ '1577' ,'<a href=coria/1577_EffenDeWeg.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Effen de weg' ], 
+[ '1602' ,'<a href=coria/1602_Wees_niet_bevreesd.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Wees niet bevreesd ' ], 
+[ '1606' ,'<a href=coria/1606_God_met_mij_op_alle_wegen.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'God met mij op alle wegen' ], 
+[ '1609' ,'<a href=coria/1609_Dank_U_Heer.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Dank u heer' ], 
+[ '1610' ,'<a href=coria/1610_lofgezongen.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'lofgezongen' ], 
+[ '1612' ,'<a href=coria/1612_Vrede_van_God.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Vrede van God' ], 
+[ '1643' ,'<a href=coria/1643_Halleluja_GodSchenktOns.html><img src="src/quaver.svg"width="20" height="20"></img></a>','<a href=pdf/SAK_1643-5_Halleluja_GodSchenktOnsDeToekomst.pdf><img src="src/Icon_PDF.png"width="20" height="20"></a>','' ,'', 'Halleluja God schenkt ons' ],
+[ '1653' ,'<a href=coria/1653_AlleluiaBereidDeWegDesHeren.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Alleluia Bereid de weg des Heren' ], 
+[ '1655' ,'<a href=coria/1655_Het_lied_van_samen.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Het lied van samen ' ], 
+[ '1658' ,'<a href=coria/1658_HoorDeHerders.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Hoor de herders ' ], 
+[ '1660' ,'<a href=coria/1660_Kerstzegen.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Kerstzegen' ], 
+[ '1670', ' ', ' ' , '<a href=https://www.youtube.com/watch?v=Kkz_14GR_D8> <img src="src/world-wide-web.png" width="20" height="20"> </a>', '' , 'Jezus diep in de woestijn'       ],
+[ '1702' ,'<a href=coria/1702_AlsInTBrood.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' <a href=pdf/SAK_1702-4_AlsInTBrood.pdf><img src="src/Icon_PDF.png"width="20" height="20"></a>','' ,'', 'Als in het brood' ],
+[ '1703' ,'<a href=coria/1703_KijkVooruit.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Kijk vooruit ' ], 
+[ '1705' ,'<a href=coria/1705_IkZoekEenLand.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Ik zoek een land' ], 
+[ '1706' ,'<a href=coria/1706_GEBED_VAN_SINT-FRANCISCUS.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Gebed van Sint-Franciscus' ], 
+[ '1710' ,'<a href=coria/1710_MijnToeverlaat.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' <a href=pdf/SAK_1710-4_MijnToeverlaat.pdf><img src="src/Icon_PDF.png"width="20" height="20"></a>','' ,'', 'Mijn toeverlaat ' ], 
+[ '1775' ,'<a href=coria/1775_TenHemelOpgevaren.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Ten hemel opgevaren' ], 
+[ '1799' ,'<a href=coria/1799_Toon_ons_uw_Zoon.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' <a href=pdf/SAK_1799-4_ToonOnsUwZoon.pdf><img src="src/Icon_PDF.png"width="20" height="20"></a>','' ,'', 'Toon ons uw Zoon' ],
+[ '1801' ,'<a href=coria/1801_Gelukkig.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' <a href=pdf/SAK_1801-4_Gelukkig.pdf><img src="src/Icon_PDF.png"width="20" height="20"></a>','' ,'', 'Gelukkig' ],
+[ '1802' ,'<a href=coria/1802_ElHimnoDeLaPaz(Chili).html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'El himno de la paz (Chili)' ], 
+[ '1902' ,'<a href=coria/1902_TheRoadGoesEverOnAndOn.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'The road goes ever on and on' ], 
+[ '1906' ,'<a href=coria/1906_Alleluia.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Alleluia' ], 
+[ '1907' ,'<a href=coria/1907_AlsHetBroodDatWijDelen.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Als het brood dat wij delen ' ], 
+[ '1914' ,'<a href=coria/1914_JesusChristYouAreMyLife.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Jesus Christ you are my life' ], 
+[ '1917' ,'<a href=coria/1917_AdoramusTe.html><img src="src/quaver.svg"width="20" height="20"></img></a>','','' ,'', 'Adoramus te' ],
+[ '1946' ,'<a href=coria/1946_DoopOnsHeer.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Doop ons heer' ], 
+[ '1949' ,'<a href=coria/1949_KyriëEleison.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' <a href=pdf/SAK_1949-4_KyriëEleison.pdf><img src="src/Icon_PDF.png"width="20" height="20"></a>','' ,'', 'Kyrië eleison' ],
+[ '1951' ,'<a href=coria/1951_Heer_verhoor_ons.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Heer verhoor ons' ], 
+[ '1954' ,'<a href=coria/1954_GoedeVaderVanAlleVolken.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Goede vader van alle volken ' ], 
+[ '1962' ,'<a href=coria/1962_AdventDatIs.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Advent dat is' ], 
+[ '1963' ,'<a href=coria/1963_FluisterDanDatJeGelooft.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Fluister dan dat je gelooft' ],
+[ '    ' ,'                                                                                                          ',' ','' ,'', '                           ' ] 
+];
+
+
+function createTable(dataFarde1300) {
+  const [headings, ...rows] = dataFarde1300;
+      
+    return `
+          <table>
+            <thead>${getCells(headings, 'th')}</thead>
+            <tbody>${rows.map(getRow).join('')}</tbody>
+          </table>
+    `;
+}
+
+document.body.insertAdjacentHTML('beforeend', createTable(dataFarde1300));
