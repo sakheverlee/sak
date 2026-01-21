c0d6d40352df3c6886ef82e24866362770f4c4f6
sakheverlee <168533060+sakheverlee@users.noreply.github.com>
2026-01-21 11:00:39 +0100

    Update script20260125.js

--- src/2026/script20260125.js
+++ src/2026/script20260125.js
@@ -4,7 +4,7 @@ let data20260125 = [
     ['nummer', 'strofen',       'coria ',                                                                                          'pdf' , 'web' , 'mp3' , 'titel'                               ],
     ['0808', '1-3',       '<a href=src/missing.html>                           <img src="src/missing.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Het volk dat wandelt in het duister' ],
     ['1486', 'alles',     '<a href=coria/1486_KomHeerJezus.html>               <img src="src/quaver.svg"  width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Kom, Heer Jezus'                       ],
-    ['0166', 'alles',     '<a href=src/missing.html>                           <img src="src/quaver.svg"  width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Eert God die onze Vader is'       ],
+    ['0166', 'alles',     '<a href=src/missing.html>                           <img src="src/missing.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Eert God die onze Vader is'       ],
     ['0897', '1-2-3',     '<a href=src/missing.html>                           <img src="src/missing.svg" width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Jezus die langs het water liep (ZJ 562)' ],
     ['0386', 'alles',     '<a href=coria/0386_DE_ZALIGSPREKENDEN.html>         <img src="src/quaver.svg"  width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Woorden van eeuwig leven'                    ],
     ['0549', 'alles',     '<a href=coria/0549_LaudateOmnesGentes.html>         <img src="src/quaver.svg"  width="20" height="20"></img> </a>',  ' ', ' ' , '' , 'Laudate omnes gentes'       ],
