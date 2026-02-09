11a9347039f170693feacd99b126430a9df3e207
sakheverlee <168533060+sakheverlee@users.noreply.github.com>
2026-02-09 12:26:53 +0100

    Update Farde0200.js

--- src/Farde0200.js
+++ src/Farde0200.js
@@ -7,8 +7,8 @@ let dataFarde0200 = [
 [ '0247' ,'<a href=coria/0247_KindOnsGeboren.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Kind ons geboren' ], 
 [ '0260' ,'<a href=coria/0260_DeGeestDesHeren.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'De geest des heren ' ], 
 [ '0266' ,'<a href=coria/0266_Here_der_Heren.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Here der Heren' ], 
-[ '0272' ,'<a href=coria/0284_JijHebtEenNaam.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Jij hebt een naam' ], 
-[ '0284' ,'<a href=coria/0272_IsDroomEenDroom.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Is droom een droom' ], 
+[ '0272' ,'<a href=coria/0272_IsDroomEenDroom.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Is droom een droom' ], 
+[ '0284' ,'<a href=coria/0284_JijHebtEenNaam.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Jij hebt een naam' ], 
 [ '0286' ,'<a href=coria/0286_GeprezenZijOnzeGod.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' ','' ,'', 'Geprezen zij onze God ' ], 
 [ '0289' ,'<a href=coria/0289_IkZieDeHemelOpengaan.html><img src="src/quaver.svg"width="20" height="20"></img></a>',' <a href=pdf/SAK_0289-4_IkZieDeHemelOpengaan.pdf><img src="src/Icon_PDF.png"width="20" height="20"></a>','' ,'', 'Ik zie de hemel opengaan ' ],
 [ '    ' ,'                                                                                                          ',' ','' ,'', '                           ' ] 
